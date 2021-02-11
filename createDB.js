241df23a59a2a1dc27d5d178bfc58ddd606a15aa
DarrenChiang <nerradchiang@gmail.com>
2021-02-11 18:30:12 -0500

    Update createDB.js

--- createDB.js
+++ createDB.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Pleasebuffthederavandal123"
+    password: "password"
 });
 
 con.connect(function(err) {
