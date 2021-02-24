fb6f2523e49079e63c6d5b1b2393b1316fc5491b
DarrenChiang <nerradchiang@gmail.com>
2021-02-24 01:06:17 -0500

    Connecting Front and Back end

--- server/lib/query.js
+++ server/lib/query.js
@@ -1,33 +1,23 @@
 /* Temporarily ignoring patient table
-    Also needs callback
-function addPatient(con, patient) {
+function addPatient(con, patient, callback) {
     var sql = "INSERT INTO db.Patients (fname, lname, birthdate, sex) VALUES ?;";
     var values = [[patient.fname, patient.lname, patient.birth, patient.sex]];
 
-    con.query(sql, [values], function(err, result) {
-        if (err) {
-            console.log('Query failed.');
-            throw err;
-        }
-
-        console.log('Patient added.');
-        con.end();         
+    con.query(sql, [values], function(err) {
+        if (err) console.log('Query failed.');
+        callback(err);
     });
 }
 
-function getPatientID(con, patient) {
+function getPatientID(con, patient, callback) {
     var sql = "SELECT id FROM db.Patients WHERE fname='" + patient.fname
             + "' AND lname='" + patient.lname
             + "' AND age='" + patient.age
             + "' AND sex='" + patient.sex + "';";
 
     con.query(sql, function(err, result) {
-        if (err) {
-            console.log('Query failed.');
-            throw err;
-        }
-
-        return result;
+        if (err) console.log('Query failed.');
+        callback(err, result);
     });
 }
 */
@@ -37,8 +27,10 @@ function getDoctors(con, callback) {
 
     con.query(sql, function(err, result) {
         if (err) {
-            console.log('Query failed to retrieve doctors.');
-            throw err;
+            console.log('Failed to retrieve list of doctors from database.');
+        } else {
+            console.log('Retrieved all of doctors from database.');
+            console.log(result);
         }
 
         callback(err, result);
@@ -50,25 +42,69 @@ function addDoctor(con, doctor, callback) {
     var values = [[doctor.fname, doctor.lname]];
 
     con.query(sql, [values], function(err) {
-        if (err) {
-            console.log('Query failed to add doctor ' + doctor.fname + ' ' + doctor.lname + '.');
-            throw err;
+        if (err) { 
+            console.log('Failed to add doctor ' + doctor.fname + ' ' + doctor.lname + ' to database.');
+        } else {
+            console.log('Added doctor ' + doctor.fname + ' ' + doctor.lname + ' to database.')
         }
 
         callback(err);
     });
 }
 
-function addAppointment(con, appointment, patient, doctor_id, callback) {
+function getAppointments(con, callback) {
+    var sql = "SELECT * FROM db.Appointments;";
+
+    con.query(sql, function(err, result) {
+        if (err) {
+            console.log('Failed to retrieve list of appointments from database.');
+        } else {
+            console.log('Retrieved all appointments from database.');
+        }
+
+        callback(err, result);
+    });
+}
+
+function getUpcomingAppointments(con, callback) {
+    var sql = "SELECT * FROM db.Appointments WHERE status!=0 ORDER BY datetime ASC;";
+
+    con.query(sql, function(err, result) {
+        if (err) {
+            console.log('Failed to retrieve list of appointments from database.');
+        } else {
+            console.log('Retrieved all appointments from database.');
+        }
+
+        callback(err, result);
+    });
+}
+
+function getLatestUpcomingAppointments(con, num, callback) {
+    var sql = "SELECT * FROM db.Appointments WHERE status!=0 ORDER BY datetime ASC LIMIT " + num + ';';
+
+    con.query(sql, function(err, result) {
+        if (err) {
+            console.log('Failed to retrieve list of appointments from database.');
+        } else {
+            console.log('Retrieved all appointments from database.');
+        }
+
+        callback(err, result);
+    });
+}
+
+function addAppointment(con, appointment, patient, doctor, callback) {
     var sql = "INSERT INTO db.Appointments (p_fname, p_lname, d_id, datetime, description, status) VALUES ?;";
-    var values = [[appointment.p_fname, p_lname, doctor_id, appointment.datetime]];
+    var values = [[appointment.p_fname, p_lname, doctor.id, appointment.datetime]];
 
     con.query(sql, [values], function(err) {
         if (err) {
-            console.log('Query failed.');
-            throw err;
-        }
-        
+            console.log('Failed to add appointment to database.'); 
+        } else {
+            console.log('Added appointment to database.');
+        }  
+
         callback(err);
     });
 }
--- server/server.js
+++ server/server.js
@@ -1,62 +1,101 @@
 const express = require('express');
-const path = require('path');
 const mysql = require('mysql');
 const query = require('./lib/query');
+const path = require('path');
 
 const app = express();
 const con = require('./cfg/mysql').con;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-//app.use(express.static(__dirname + 'dir of public folder'));
 
-// TESTING START
+const office = __dirname + '/../client/office/';
+
+app.use('/assets', express.static(office + 'assets'));
+
+// TESTING
 app.get('/testing', function(req, res) {
 	res.sendFile(path.join(__dirname + '/testing.html'));
 });
 
+app.get('/', function(req, res) {
+	res.redirect('/home');
+});
+
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(office + 'index.html'));
+});
+
+app.get('/calendar.js', function(req, res) {
+    res.sendFile(path.join(office + 'calendar.js'));
+});
+
+app.get('/profile', function(req, res) {
+	res.sendFile(path.join(office + 'profile.html'));
+});
+
+app.get('/appointments', function(req, res) {
+	res.sendFile(path.join(office + 'appointments.html'));
+});
+
+app.get('/appointmentListing', function(req, res) {
+	res.sendFile(path.join(office + 'appointmentListing.html'));
+});
+
+app.get('/appointmentListing.js', function(req, res) {
+	res.sendFile(path.join(office + 'appointmentListing.js'));
+});
+
+app.get('/delays', function(req, res) {
+	res.sendFile(path.join(office + '/delays'));
+});
+
+app.get('/upcomingAppointments', function(req, res) {
+    res.sendFile(path.join(office + 'upcomingAppointments.html'));
+});
+
+app.get('/upcomingAppointments.js', function(req, res) {
+    res.sendFile(path.join(office + 'upcomingAppointments.js'));
+});
+
 app.get('/getDoctors', function(req, res) {
 	query.getDoctors(con, function(err, result) {
-		res.send(result);
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
 	});
 });
 
 app.post('/addDoctor', function(req, res) {
 	query.addDoctor(con, req.body, function(err) {
 		query.getDoctors(con, function(err, result) {
-			res.send(result);
+			if (err) {
+				console.log(err);
+			} else {
+				res.send(result);
+			}			
 		});
 	});
 });
-// TESTING END
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../client/office/index.html'));
+app.listen(8080, function(err) {
+	if (err) {
+		throw err;
+	}
+
+	console.log('Server is listening on port 8080.');
 });
 
-app.get('/css/main.css', function(req, res) {
-	res.sendFile(path.join(__dirname + '/../client/office/css/main.css'));
+con.connect(function(err) {
+	if (err) {
+		console.log('Failed to connect to database.');
+		console.log(err);
+	} else {
+		console.log('Connected to database.');
+	}	
 });
 
-app.get('/images/person-icon.png', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../client/office/images/person-icon.png'));
-});
-
-app.get('/calendar.js', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../client/office/calendar.js'));
-});
-
-app.get('/upcomingAppointments', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../client/office/upcomingAppointments.html'));
-});
-
-app.get('/upcomingAppointmentsStyle.css', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../client/office/upcomingAppointmentsStyle.css'));
-});
-
-app.listen(8080);
-console.log('Server is listening on port 8080.');
-con.connect();
-console.log('Connected to database.');
 
 
