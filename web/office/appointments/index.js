c9bc99fde8a8d193b629170ded4a6c78b8abf0eb
DarrenChiang <nerradchiang@gmail.com>
2021-03-16 18:22:34 -0400

    todo comment

--- web/office/appointments/index.js
+++ web/office/appointments/index.js
@@ -12,5 +12,9 @@ urlParams.forEach(function(value, key) {
 	filters[key] = value;
 });
 
+if (filters.date) {
+	//indicate date in title of list using REACT
+}
+
 var appointments = new Dynamic_List('appointments', render_appointment_item);
 send_request(filters);
