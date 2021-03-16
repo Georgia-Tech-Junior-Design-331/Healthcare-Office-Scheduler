d2b9d4635bae4582b2d73a02c70e5818ce022e99
DarrenChiang <nerradchiang@gmail.com>
2021-03-16 18:04:22 -0400

    restored calendar to app list func

--- web/office/appointments/index.js
+++ web/office/appointments/index.js
@@ -1,9 +1,16 @@
-function send_request(filters) {
+function send_request(filters) {	
 	const request = '/getAppointments';
 	const body = {filters: filters};
 	appointments.request_items(request, body);
 }
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 var filters = {};
+
+urlParams.forEach(function(value, key) {
+	filters[key] = value;
+});
+
 var appointments = new Dynamic_List('appointments', render_appointment_item);
 send_request(filters);
